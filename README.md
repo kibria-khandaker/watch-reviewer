be0f4c10796bffbdb06663f96cb2064434c5238f
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-04-04 18:01:19 +0600

    make custom hook file

--- README.md
+++ README.md
@@ -4,13 +4,13 @@
 
 ### My project Live link, And click [Here](https://vermillion-cocada-8ce3b2.netlify.app/) to view it in your browser.
 
-###  Or, Copy/Click the link [https://vermillion-cocada-8ce3b2.netlify.app/](https://vermillion-cocada-8ce3b2.netlify.app/)
+### Or, Copy/Click the link [https://vermillion-cocada-8ce3b2.netlify.app/](https://vermillion-cocada-8ce3b2.netlify.app/)
 
 <br /><br /><br />
 
 > #### Some notes about My (Watch Reviewer) React Project
 
-- manege for navigation i used 'react-router-dom'
-- For all kind of icon i used 'react-icons'
-- i used 'Recharts' for DashBoard chart
-- Blog page used some image, which is i used from google others websites.
+- Manege for the data i used 'Custom Hook for Reviews' and 'Context API for DashBoard Chart'
+- Manege for navigation i used 'react-router-dom'
+- I used 'Recharts' for DashBoard chart, And For all kind of icon i used 'react-icons'
+- Blog page's 2 images, which is i used from google's others websites.
--- src/components/Header/NavLists.js
+++ src/components/Header/NavLists.js
@@ -5,10 +5,10 @@ const NavLists = () => {
     return (
         <nav className='menuList flex flex-col md:flex-row  gap-4 text-xl'>
             <NavLink className={({ isActive }) => isActive ? 'text-cyan-500 underline decoration-wavy' : ' text-black'} to="/">Home</NavLink>
-            <NavLink className={({ isActive }) => isActive ? 'text-cyan-500 underline decoration-wavy' : ' text-black'} to="/reviews">reviews</NavLink>
-            <NavLink className={({ isActive }) => isActive ? 'text-cyan-500 underline decoration-wavy' : ' text-black'} to="/dashboard">dashboard</NavLink>
-            <NavLink className={({ isActive }) => isActive ? 'text-cyan-500 underline decoration-wavy' : ' text-black'} to="/blogs">blogs</NavLink>
-            <NavLink className={({ isActive }) => isActive ? 'text-cyan-500 underline decoration-wavy' : ' text-black'} to="/about">about</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'text-cyan-500 underline decoration-wavy' : ' text-black'} to="/reviews">Reviews</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'text-cyan-500 underline decoration-wavy' : ' text-black'} to="/dashboard">Dashboard</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'text-cyan-500 underline decoration-wavy' : ' text-black'} to="/blogs">Blogs</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'text-cyan-500 underline decoration-wavy' : ' text-black'} to="/about">About</NavLink>
         </nav>
     );
 };
